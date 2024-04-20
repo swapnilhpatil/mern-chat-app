4362b70391d6e0f50ec72176731850f96daf1f68
Swapnil <swapnilhpatil9999@gmail.com>
2024-04-21 01:48:20 +0530

    create basic REST API routes for chat

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,24 @@ app.get("/", (req, res)=> {
    res.send("Hello World!");
 });
 
+app.get("/api/chat", ()=>{
+   //get all chat data
+});
+
+app.get("/api/chat/:id", (req, res)=>{
+    //get specific chat data using id
+});
+app.post("api/chat", (req, res) => {
+  // create new chat
+});
+app.put("/api/chat/:id",(req,res)=> {
+   //update chat using id
+});
+
+app.delete("/api/chat/:id",(req,res)=>{
+    //delete cha using id
+});
+
 app.listen(PORT, ()=>{
     console.log(`App listing on port: ${PORT}`);
 });
